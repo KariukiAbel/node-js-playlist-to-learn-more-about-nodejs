ba587c2b4f9309ff428ea3a311d5085b715232e0
KariukiAbel <abelmuriithi@gmail.com>
2022-04-24 09:21:02 +0300

    Learning some node js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var time = 0;
+var count = require('./count')
 
 // setInterval(function(){
 //   time+=2;
 //   console.log(time + " seconds have passed");
 // }, 2000);
 
-var timer = setInterval(function(){
-  time+=2;
-  console.log(time + " seconds have passed");
-  if(time > 5){
-    clearInterval(timer)
-  }
-}, 2000);
+// var timer = setInterval(function(){
+//   time+=2;
+//   console.log(time + " seconds have passed");
+//   if(time > 5){
+//     // clearInterval(timer)
+//   }
+// }, 2000);
 
 //printing the current directory name
 console.log(__dirname);
 
 //printing the absolute path of my file
 console.log(__filename);
+console.log(count(['abel', 'james', 'jane']));
---
+++ count.js
@@ -0,0 +1,4 @@
+function count(arr[]){
+  return "There are " + arr.length + " items in the array"
+}
+module.exports = count;
