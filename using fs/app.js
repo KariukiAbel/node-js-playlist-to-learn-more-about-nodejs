274a4265c7bee6e1c6415d7b31741589f1377f7a
KariukiAbel <abelmuriithi@gmail.com>
2022-04-25 21:32:54 +0300

    Added the unlink method from the fs module used for deleting files

--- using fs/app.js
+++ using fs/app.js
@@ -10,6 +10,13 @@ fs.writeFileSync('new textfile.txt', fileRead)
 // reading and writing a file asynchroniously
 fs.readFile('new textfile.txt', 'utf8', function (err, data) {
 //callback function fired when the proces is done
+if (err) {
+console.log(err);
+}
+fs.writeFile('A new TxtFile.txt', data);
 console.log(data);
 })
 console.log('This line will be executed first before the output of the file reading');
+
+//deleting a file
+fs.unlink('new textfile.txt');
