d3555599d240516dd7d77421dea238d9e12cccf4
KariukiAbel <abelmuriithi@gmail.com>
2022-04-25 21:07:27 +0300

    The best illustration of blocking and unblocking

--- using fs/app.js
+++ using fs/app.js
@@ -12,3 +12,4 @@ fs.readFile('new textfile.txt', 'utf8', function (err, data) {
 //callback function fired when the proces is done
 console.log(data);
 })
+console.log('This line will be executed first before the output of the file reading');
