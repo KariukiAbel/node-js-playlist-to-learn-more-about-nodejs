5d69919e4347e3b8ffcf03214592ec7dd41a95b6
KariukiAbel <abelmuriithi@gmail.com>
2022-04-29 13:14:47 +0300

    Creating readable streams and getting to know streams and buffers. Buffer is a storage area while a stream is a flow of data. On the output it displays what is in the buffer and not the actual text

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-  console.log('request was made from '+ req.url);
-res.writeHead(200, {'Content-Type':'text/plain'})
-res.end("This is node js")
-})
 
-server.listen(3000, 'localhost');
+const readStream = fs.createReadStream(__dirname + '/text.txt')
+readStream.on('data',(chunk)=> {
+  console.log('new chunk received: ')
+   console.log(chunk);
+ })
+
+// const server = http.createServer((req, res)=>{
+//   console.log('request was made from '+ req.url);
+// res.writeHead(200, {'Content-Type':'text/plain'})
+// res.end("This is node js")
+// })
+
+// server.listen(3000, 'localhost');
 console.log('Listening to port 3000');
