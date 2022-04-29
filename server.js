4aeb3b39b4f5ed22a6fa60c65d49eba137e200ed
KariukiAbel <abelmuriithi@gmail.com>
2022-04-29 13:47:14 +0300

    Just used a one liner, that is used pipe to do what I had done before, creating a readStream and writeStream

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const fs = require('fs');
 const readStream = fs.createReadStream(__dirname + '/text.txt','utf8')
 const writeStream = fs.createWriteStream(__dirname + '/text2.txt')
 
-readStream.on('data',(chunk)=> {
-  console.log('new chunk received: ')
-   // console.log(chunk);
-   writeStream.write(chunk)
- })
+// readStream.on('data',(chunk)=> {
+//   console.log('new chunk received: ')
+//    // console.log(chunk);
+//    writeStream.write(chunk)
+//  })
+
+
+// using pipe used on readable streams
+readStream.pipe(writeStream)
 
 // const server = http.createServer((req, res)=>{
 //   console.log('request was made from '+ req.url);
