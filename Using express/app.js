4648de814b4ffd167e2513382af84737345af980
KariukiAbel <abelmuriithi@gmail.com>
2022-05-12 00:00:37 +0300

    Changed the main line on package.json to point at app.js in the Using express directory which I created to learn all things about express before starting my own project

---
+++ Using express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// setting up our express app
+const app = express()
+
+app.get('/', (req,res)=>res.send('This is the homepage'))
+
+app.get('/contact', (req,res)=>res.send('This is the contact page'))
+
+app.listen(3000)
