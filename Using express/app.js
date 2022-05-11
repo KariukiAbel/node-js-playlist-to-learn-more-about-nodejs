5677aa1638175897f59e25d8762077ebcf0f7b1f
KariukiAbel <abelmuriithi@gmail.com>
2022-05-12 00:08:43 +0300

    Using express route params to get data dynamically

--- Using express/app.js
+++ Using express/app.js
@@ -7,4 +7,6 @@ app.get('/', (req,res)=>res.send('This is the homepage'))
 
 app.get('/contact', (req,res)=>res.send('This is the contact page'))
 
+app.get('/profile/:id', (req,res)=> res.send('You request to see the profile with the id of ' + req.params.id))
+
 app.listen(3000)
