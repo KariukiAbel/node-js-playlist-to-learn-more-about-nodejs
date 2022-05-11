53045c708941a1a72b5627502b5f2198dd42ee5d
KariukiAbel <abelmuriithi@gmail.com>
2022-05-12 00:23:44 +0300

    sending html file as a respose. Got an error. It seems I am not able to render the html files as of now

--- Using express/app.js
+++ Using express/app.js
@@ -3,9 +3,16 @@ const express = require('express');
 // setting up our express app
 const app = express()
 
-app.get('/', (req,res)=>res.send('This is the homepage'))
+app.get('/', (req,res)=>{
+  // res.send('This is the homepage')
+  var index = '../index.html'
+  res.sendFile(index)
+})
 
-app.get('/contact', (req,res)=>res.send('This is the contact page'))
+app.get('/contact', (req,res)=>{
+// res.send('This is the contact page')
+res.sendFile('../contact.html')
+})
 
 app.get('/profile/:id', (req,res)=> res.send('You request to see the profile with the id of ' + req.params.id))
 
