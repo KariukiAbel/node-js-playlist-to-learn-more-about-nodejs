9ada476c9532f6fffa750f31b21357ae8b9d94fd
KariukiAbel <abelmuriithi@gmail.com>
2022-05-12 00:47:37 +0300

    Dynamically adding data to a view through the ejs template engine

--- Using express/app.js
+++ Using express/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 // setting up our express app
 const app = express()
 
+// when templates are requested, by default they will be found
+ // at the views folder according to ejs
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res)=>{
   // res.send('This is the homepage')
   var index = '../index.html'
@@ -14,6 +18,16 @@ app.get('/contact', (req,res)=>{
 res.sendFile('../contact.html')
 })
 
-app.get('/profile/:id', (req,res)=> res.send('You request to see the profile with the id of ' + req.params.id))
+app.get('/profile/:id', (req,res)=>res.send('You request to see the profile with the id of ' + req.params.id))
+
+app.get('/profile/:name', (req,res)=> {
+  // res.send('You request to see the profile with the name of ' + req.params.name)
+// rendering the profile.ejs view in views folder
+var data ={
+  age:24,
+  job: 'Software developer and engineer'
+}
+res.render('profile', {person: req.params.name, data})
+})
 
 app.listen(3000)
