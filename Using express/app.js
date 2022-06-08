5ccdd66216e4474a4433920e24f15cd7523287f5
KariukiAbel <abelmuriithi@gmail.com>
2022-06-08 18:59:52 +0300

    Getting SyntaxError: Unexpected identifier in /home/nabesh/AtomProjects/node js/node js playlist/views/profile.ejs while compiling ejs
    If the above error is not helpful, you may want to try EJS-Lint:
    https://github.com/RyanZim/EJS-Lint
    Or, if you meant to create an async function, pass  as an option.

--- Using express/app.js
+++ Using express/app.js
@@ -4,31 +4,35 @@ const express = require('express');
 const app = express()
 
 // when templates are requested, by default they will be found
- // at the views folder according to ejs
-app.set('view engine', 'ejs')
+// at the views folder according to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', (req,res)=>{
-  // res.send('This is the homepage')
-  res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/index.html')
-})
+app.get('/', (req, res) => {
+    // res.send('This is the homepage')
+    res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/index.html')
+});
 
-app.get('/contact', (req,res)=>{
-// res.send('This is the contact page')
-res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/contact.html')
-})
+app.get('/contact', (req, res) => {
+    // res.send('This is the contact page')
+    res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/contact.html')
+});
 
 // app.get('/profile/:id', (req,res)=>res.send('You request to see the profile with the id of ' + req.params.id))
 
-app.get('/profile/:name', (req,res)=> {
-  // res.send('You request to see the profile with the name of ' + req.params.name)
-// rendering the profile.ejs view in views folder
-var data ={
-  age:24,
-  job: 'Software developer and engineer',
-  hobbies:['eating','sleeping','coding', 'travelling', 'reading','driving']
-}
-console.log(req.params.name);
-res.render('profile', {person: req.params.name, data})
-})
+app.get('/profile/:name', (req, res) => {
+    // res.send('You request to see the profile with the name of ' + req.params.name)
 
-app.listen(3000)
+    // rendering profile.ejs view in views folder
+    var data = {
+        age: 24,
+        job: 'Software developer and engineer',
+        hobbies: ['eating', 'sleeping', 'coding', 'traveling', 'reading', 'driving']
+      }
+
+    // console.log(req.params.name);
+
+    res.render('profile', { person: req.params.name, data });
+
+});
+
+app.listen(3000);
