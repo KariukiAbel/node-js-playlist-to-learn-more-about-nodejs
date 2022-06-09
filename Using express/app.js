616be4acd08a7cb392e214cb59bd03f7aa939255
KariukiAbel <abelmuriithi@gmail.com>
2022-06-09 17:46:42 +0300

    Trying to add static files

--- Using express/app.js
+++ Using express/app.js
@@ -10,17 +10,32 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    // res.send('This is the homepage')
-    res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/index.html')
-});
+// using middlewares and static files
+app.use('/assets', express.static('assets'))
 
-app.get('/contact', (req, res) => {
-    // res.send('This is the contact page')
-    res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/contact.html')
-});
-
+// old way of rendering html files
+// app.get('/', (req, res) => {
+//     // res.send('This is the homepage')
+//     res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/index.html')
+// });
+//
+// app.get('/contact', (req, res) => {
+//     // res.send('This is the contact page')
+//     res.sendFile('/home/nabesh/AtomProjects/node js/node js playlist/contact.html')
+// });
+//
 // app.get('/profile/:id', (req,res)=>res.send('You request to see the profile with the id of ' + req.params.id))
+
+
+// new way of rendering ejs files
+app.get('/', (req,res)=>{
+  res.render('index')
+})
+
+app.get('/contact', (req,res)=>{
+  res.render('contact')
+})
+
 
 app.get('/profile/:name', (req, res) => {
     // res.send('You request to see the profile with the name of ' + req.params.name)
